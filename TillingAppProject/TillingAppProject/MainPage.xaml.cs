2c90c39fd44eb3cad338408aaa2058ce40b312cc
unknown <cormacmchale1@gmail.com>
2017-11-14 10:50:38 +0000

    added the first function to cs

--- TillingAppProject/TillingAppProject/MainPage.xaml.cs
+++ TillingAppProject/TillingAppProject/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,16 +23,52 @@ namespace TillingAppProject
     /// </summary>
     public sealed partial class MainPage : Page
     {
- 
-
-    public MainPage()
+        public MainPage()
         {
             this.InitializeComponent();
         }
 
         private void generateGrid_Tapped(object sender, TappedRoutedEventArgs e)
         {
+            addTilingGrid();
+        }
 
+        private void addTilingGrid()
+        {
+            // remove the old grid
+            try
+            {
+                rootGrid.Children.Remove(FindName("tileGrid") as Grid);
+            }
+            catch { }
+            int sizeofgrid = Convert.ToInt32(SizeOfGrid.Text.ToString());
+            int sizeofTile = Convert.ToInt32(SizeOfTiles.Text.ToString());
+            //create grid object
+            Grid tileTemplate = new Grid();
+            //name, size, allignment,, bacrkound colour
+            tileTemplate.Name = "tileGrid";
+            tileTemplate.HorizontalAlignment = HorizontalAlignment.Center;
+            tileTemplate.VerticalAlignment = VerticalAlignment.Top;
+            //tileTemplate.Height = _rowHeight; //100 * _rows;
+            //tileTemplate.Width = +_rowHeight;// 100 * _rows;
+            tileTemplate.Background = new SolidColorBrush(Colors.Gray);
+            tileTemplate.Margin = new Thickness(5);
+            tileTemplate.SetValue(Grid.RowProperty, 1);
+            //tileTemplate.Background = {ApplicationTheme};
+            tileTemplate.Height = sizeofgrid; //100 * _rows;
+            tileTemplate.Width = +sizeofgrid;// 100 * _rows;
+            tileTemplate.Background = new SolidColorBrush(Colors.Gray);
+            tileTemplate.Margin = new Thickness(5);
+            tileTemplate.SetValue(Grid.RowProperty, 1);
+
+            //add row and column definitions
+            for (int i = 0; i < sizeofgrid; i++)
+            {
+                tileTemplate.RowDefinitions.Add(new RowDefinition());
+                tileTemplate.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            // add chessboard to root grid
+            rootGrid.Children.Add(tileTemplate);
         }
     }
 }
